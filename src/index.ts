ef84c74b10db751e443a3171e75e3d82aaf95f65
Lelsey Dreyer <lesleydreyer@hotmail.com>
2020-04-16 09:50:55 -0700

    sections 87-89

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-
-// pass in obj w methods that are different for each type of collection
 const sorter1 = new Sorter(numbersCollection);
 sorter1.sort();
 console.log(numbersCollection.data);
@@ -13,3 +12,12 @@ const charactersCollection = new CharactersCollection('Xaayb');
 const sorter2 = new Sorter(charactersCollection);
 sorter2.sort();
 console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+const sorter3 = new Sorter(linkedList);
+sorter3.sort();
+linkedList.print();
